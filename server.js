0907a981a90138d95eb4dd10ef7a02c567848d72
Aashish Tiwari <aashishtiwari97@gmail.com>
2017-08-15 16:34:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,73 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
+
+var articles ={
+    articleOne:{
+        title:'Article-one|Aashish Tiwar',
+        heading:'Article One',
+        date:'Aug 14, 2016',
+        content:
+        `<p>
+             Here I have created this web page to learn the basics of the web developement. Here I have created this web page to learn the basics of the web developement. Here I have created this web page to learn the basics of the web developement. Here I have created this web page to learn the basics of the web developement. Here I have created this web page to learn the basics of the web developement.
+        </p>
+        
+        <p>
+             Hurrah !! I have created this web page to learn the basics of the web developement. Here I have created this web page to learn the basics of the web developement. Here I have created this web page to learn the basics of the web developement. Here I have created this web page to learn the basics of the web developement. Here I have created this web page to learn the basics of the web developement.
+        </p>
+        `
+    }
+};
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmlTemplate= `
+            <html>
+                <head>
+                    <title>
+                       ${title}
+                    </title>
+                    <meta name="viewport" content="width-device-width,initial-scale-1" />
+                    <link href="/ui/style.css" rel="stylesheet" />
+                </head>
+                <body>
+                    <div class="container">
+                        
+                        <div>
+                            <a href="/">Home</a>
+                        </div>
+                        <hr/>
+                        <h3>
+                            ${heading}
+                        </h3>
+                        <div>
+                            ${date}
+                        </div>
+                         <div>
+                            ${content}
+                        </div>
+                </div>
+                    
+                </body>
+                
+            </html>
+            
+            `;
+   return htmlTemplate; 
+}
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
