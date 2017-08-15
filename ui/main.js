db3d74e1d882a5d58da10dff37f0632aa5f13463
Aashish Tiwari <aashishtiwari97@gmail.com>
2017-08-16 00:44:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var img = document.getElementById('madi');
 var marginleft = 0;
 function moveRight(){
     marginleft = marginleft + 10;
-    img.style.marginleft + 'px';
+    img.style.marginleft= marginleft + 'px';
 }
-img.onclick= function(){
+img.onclick= function() {
     var interval = setInterval(moveRight , 100);
 };
