f2237877b7486b4ff717397a6d93d050141bba3a
Aashish Tiwari <aashishtiwari97@gmail.com>
2017-08-18 18:02:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,20 +5,20 @@ button.onclick = function(){
     //create a request..
     var request = new XMLHttpRequest();
     //capture the response and store it in a variable.
-request.onreadystatechange = function() {
-    // Process the server response here.
-            if (request.readyState === XMLHttpRequest.DONE) {
-                
-                 if (request.status === 200){
-                    var counter =  request.respnoseText;
-                    
-                    span.innerHTML= counter.toString();
-                 }
-            } 
-    //request not done yet;
-    };
+        request.onreadystatechange = function() {
+            // Process the server response here.
+                    if (request.readyState === XMLHttpRequest.DONE) {
+                        
+                         if (request.status === 200){
+                            var counter =  request.respnoseText;
+                            
+                            span.innerHTML= counter.toString();
+                         }
+                    } 
+            //request not done yet;
+            };
        //make a request.
      request.open('GET', 'http://aashishtiwari97.imad.hasura-app.io/counter', true);
      request.send(null);
         
-    };
+};
