e7580c6239f8083a6ba5deab056ab2718f638c06
Aashish Tiwari <aashishtiwari97@gmail.com>
2017-08-30 13:19:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,9 @@ app.post('/login',function(req,res){
                 if(hashedPassword === dbString){
                     res.send('Credentials correct !'); 
                 }
-                
+                else{
+                    res.send(403).send('username/password is invalid');
+                }
             }
               
            }  
