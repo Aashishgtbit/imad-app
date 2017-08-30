50568f4dcde1e4ff92bb12a44cd30c85eaa9c15e
Aashish Tiwari <aashishtiwari97@gmail.com>
2017-08-30 14:26:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,5 @@
-//counter code
-    var button = document.getElementById('counter');
 
-button.onclick = function() {
-    
-    //create a request..
-    var request = new XMLHttpRequest();
-    //capture the response and store it in a variable.
-        request.onreadystatechange = function() {
-                    if (request.readyState === XMLHttpRequest.DONE) {
-                        if (request.status === 200){
-                            var counter =  request.responseText;
-                            var span = document.getElementById('count');
-                            span.innerHTML= counter.toString();
-                         }
-                    } 
-            //request not done yet;
-            };
-    //make a request.
-    
-
-     request.open('GET', 'http://aashishtiwari97.imad.hasura-app.io/counter', true);
-     request.send(null);
-        
-};
-//submit name
+//submit username/password to login.
 
 var submit = document.getElementById('submit_btn');
 submit.onclick= function() {
@@ -34,23 +10,24 @@ submit.onclick= function() {
         request.onreadystatechange = function() {
                     if (request.readyState === XMLHttpRequest.DONE) {
                         if (request.status === 200){
-                            var names =  request.responseText;
-                            names = JSON.parse(names);
-                            var list = '';
-                            for(var i =0;i<names.length;i++){
-                                list += '<li>' + names[i] + '</li>';
-                            }
-                            var ul = document.getElementById('namelist');
-                            ul.innerHTML = list;
+                            console.log('user logged in');
+                            alert(' Logged in successfully');
+                         }else if(request.status === 403){
+                             alert('username/password is incorrect');
+                         }else if(request.status === 500){
+                             alert('Something went wrong!');
                          }
                     } 
             //request not done yet;
             };
     //make a request.
-        var nameInput = document.getElementById('name');
-        var name = nameInput.value;
-        request.open('GET', 'http://aashishtiwari97.imad.hasura-app.io/submit-name?name=' + name, true);
-        request.send(null);
+        var username = document.getElementById('username');
+         var password = document.getElementById('password');
+        console.log(username);
+        console.log(password);
+        request.open('POST', 'http://aashishtiwari97.imad.hasura-app.io/login', true);
+        request.setRequestHeader('Content-Type','application/json');
+        request.send(JSON.stringify({username:username,password:password}));
         
     //capture a list of names and render it as list.
    
