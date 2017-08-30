35edf976b2f4507493ef18d326b130e0ed495df0
Aashish Tiwari <aashishtiwari97@gmail.com>
2017-08-30 14:38:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ submit.onclick= function() {
         console.log(password);
         request.open('POST', 'http://aashishtiwari97.imad.hasura-app.io/login', true);
         request.setRequestHeader('Content-Type','application/json');
-        request.send(JSON.stringify({username:username,password:password}));
+        request.send(JSON.stringify({username:username , password:password}));
         
     //capture a list of names and render it as list.
    
